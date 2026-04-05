7fc95706b366a5af44d1fcb88421856bb366854e
Evelorion <evelorion@example.com>
2026-04-05 22:30:14 +0800

    docs: update README with v2.0 intelligent features

--- fix_magic_strings.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main() async {
-  final file = File('lib/src/ui_magic_bar.dart');
-  var text = await file.readAsString();
-  text = text.replaceAll(r'\${', r'${');
-  await file.writeAsString(text);
-  print("Fixed ui_magic_bar.dart backslashes");
-}
